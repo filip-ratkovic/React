e39f0ed8d2dc97bb1de7d77f22bd937cc164e3bd
Filip <filip.ratkovic.fr@gmail.com>
2022-08-01 22:05:42 +0200

    Vezba

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import Calculator from "./components/Calculator"
-import Tours from "./components/Tours"
 import "./App.css";
 const App = () => {
- return (
-<Tours/>
-    )
+ return 
 };
 
 export default App;
--- src/components/vezba.jsx
+++ src/components/vezba.jsx
@@ -4,45 +4,46 @@ import axios from "axios"
 const Vezba = () => {
     const axios = require("axios");
 
-const options = {
-  method: 'GET',
-  url: 'https://numbersapi.p.rapidapi.com/6/21/date',
-  params: {fragment: 'true', json: 'true'},
-  headers: {
-    'X-RapidAPI-Key': '40b2a63a8cmsh465bf0c5b2ab485p1add32jsnf62aaa87247d',
-    'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
-  }
-};
+    const options = {
+        method: 'GET',
+        url: 'https://numbersapi.p.rapidapi.com/6/21/date',
+        params: { fragment: 'true', json: 'true' },
+        headers: {
+            'X-RapidAPI-Key': '40b2a63a8cmsh465bf0c5b2ab485p1add32jsnf62aaa87247d',
+            'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
+        }
+    };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-const [data, setData] = useState([]);
-const [inputedDate, setInputedDate] = useState([])
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+    const [data, setData] = useState([]);
+    const [inputedDate, setInputedDate] = useState([])
 
-const  fetchNumberData = async () => {
-    const res = await axios.request(options).catch((error) => console.log(error));
-    setData(res);
-}
+    const fetchNumberData = async () => {
+        const res = await axios.request(options).catch((error) => console.log(error));
+        setData(res);
+    }
 
 
-useEffect(() => {
-    fetchNumberData()
-},[])
+    useEffect(() => {
+        fetchNumberData()
+    }, [])
 
-const search = () => {
-options.url="https://numbersapi.p.rapidapi.com/" + inputedDate + "/date"
-fetchNumberData()
-}
+    const search = () => {
+        options.url = "https://numbersapi.p.rapidapi.com/" + inputedDate + "/date"
+        fetchNumberData()
+    }
 
-return (
-    <div>
-        <input type="text" placeholder="Datum" onChange={(input)=> setData(input.target.value)}/>
-        <button onClick={search}>click</button>
-    </div>
-)
+    return (
+        <div>
+            <input type="text" placeholder="Datum" onChange={(input) => setData(input.target.value)} />
+            <button onClick={search}>click</button>
+
+        </div>
+    )
 }
 
 export default Vezba;
