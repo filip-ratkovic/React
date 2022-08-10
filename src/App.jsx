eb346f8f5186885866fe6c2ec553e476d436f4b8
Filip <filip.ratkovic.fr@gmail.com>
2022-08-10 12:44:23 +0200

    Vezba

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,54 @@
 import { useState, useEffect } from "react";
 import React from "react";
 import axios from "axios"
-const Vezba = () => {
+const App = () => {
     const axios = require("axios");
+const [newsData, setNewsData] = useState("");
+let inputedDate = "";
 
-    const options = {
-        method: 'GET',
-        url: 'https://numbersapi.p.rapidapi.com/6/21/date',
-        params: { fragment: 'true', json: 'true' },
-        headers: {
-            'X-RapidAPI-Key': '40b2a63a8cmsh465bf0c5b2ab485p1add32jsnf62aaa87247d',
-            'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
-        }
-    };
-
-    axios.request(options).then(function (response) {
-        console.log(response.data);
-    }).catch(function (error) {
-        console.error(error);
-    });
-    const [data, setData] = useState([]);
-    const [inputedDate, setInputedDate] = useState([])
-
-    const fetchNumberData = async () => {
-        const res = await axios.request(options).catch((error) => console.log(error));
-        setData(res);
+const options = {
+    method: 'GET',
+    url: 'https://numbersapi.p.rapidapi.com/6/21/date',
+    params: { fragment: 'true', json: 'true' },
+    headers: {
+        'X-RapidAPI-Key': '40b2a63a8cmsh465bf0c5b2ab485p1add32jsnf62aaa87247d',
+        'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
     }
+};
 
+axios.request(options).then(function (response) {
+    console.log(response.data);
+}).catch(function (error) {
+    console.error(error);
+});
 
-    useEffect(() => {
-        fetchNumberData()
-    }, [])
-
-    const search = () => {
-        options.url = "https://numbersapi.p.rapidapi.com/" + inputedDate + "/date"
-        fetchNumberData()
-
-    }
-
-    return (
-        <div>
-            <input type="text" placeholder="Datum" onChange={(input) => setData(input.target.value)} />
-            <button onClick={<div>
-                <h1>{data.text}</h1>
-            </div>}>click</button>
-            
-
-        </div>
-    )
+const fetchNewsData = async () => {
+    const res = await axios.request(options).catch((error) => console.log(error));
+   setNewsData(res.data)
 }
 
-export default Vezba;
+useEffect(() => {
+fetchNewsData()
+},[])
+
+const search = () => {
+    options.url = "https://numbersapi.p.rapidapi.com/" + inputedDate + "/date"
+    fetchNewsData()
+}
+
+return (
+    <div>
+        <input type="text" placeholder="Datum" onChange={(input) =>inputedDate= input.target.value} />
+        <button onClick={search}>click</button>
+
+        <div>
+            <h1>{newsData.year}</h1>
+            <h4>{newsData.text}</h4>
+
+        </div>
+    </div>
+    
+)
+}
+
+export default App;
