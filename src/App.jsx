960655e5bea3facfbc449ba8a8c837adeadabf96
Filip <filip.ratkovic.fr@gmail.com>
2022-08-11 23:54:33 +0200

    movie

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,26 @@
 import { useState, useEffect } from "react";
 import React from "react";
-import axios from "axios"
+import Movie from "./Movie/Movie";
+import {Route, Routes, Link} from "react-router-dom";
+import Tours from "./components/ToursCard/Tours";
+import TourCard from "./components/ToursCard/TourCard"
+
 const App = () => {
-    const axios = require("axios");
-const [newsData, setNewsData] = useState("");
-let inputedDate = "";
-
-const options = {
-    method: 'GET',
-    url: 'https://numbersapi.p.rapidapi.com/6/21/date',
-    params: { fragment: 'true', json: 'true' },
-    headers: {
-        'X-RapidAPI-Key': '40b2a63a8cmsh465bf0c5b2ab485p1add32jsnf62aaa87247d',
-        'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
-    }
-};
-
-axios.request(options).then(function (response) {
-    console.log(response.data);
-}).catch(function (error) {
-    console.error(error);
-});
-
-const fetchNewsData = async () => {
-    const res = await axios.request(options).catch((error) => console.log(error));
-   setNewsData(res.data)
-}
-
-useEffect(() => {
-fetchNewsData()
-},[])
-
-const search = () => {
-    options.url = "https://numbersapi.p.rapidapi.com/" + inputedDate + "/date"
-    fetchNewsData()
-}
-
-return (
-    <div>
-        <input type="text" placeholder="Datum" onChange={(input) =>inputedDate= input.target.value} />
-        <button onClick={search}>click</button>
+    return(
 
         <div>
-            <h1>{newsData.year}</h1>
-            <h4>{newsData.text}</h4>
+            <div>
+               <Link to="/home"><button>Home</button></Link>
+               <Link to="/tours"><button>Tourse</button></Link> 
 
-        </div>
-    </div>
-    
-)
+            </div>
+        <Routes>
+        <Route path={"/"}  element={<h1>Welcome</h1>}/>
+        <Route path={"/home"}  element={<h1>Gde si dariseee</h1>}/>
+        <Route path={"/tours"}  element={<TourCard/>}/>
+    </Routes>
+    </div> 
+        )
 }
 
 export default App;
---
+++ src/Movie/Movie.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Route, Routes, Link} from "react-router-dom";
+import { useState, useEffect } from "react";
+
+export default function Movie() {
+  return (
+    <div className="conteiner">
+        <nav className="nav">
+            
+        </nav>
+    </div>
+  )
+}
