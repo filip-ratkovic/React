5c0fe2d9ac16186b29a0326ca0939b989cfa5476
Filip <filip.ratkovic.fr@gmail.com>
2022-09-21 14:42:54 +0200

    Typescript

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import { useState, useEffect, useContext } from "react";
 import React from "react";
-import {Context} from "./UserContext"
 
 const App = () => {
-    const {user, setUser} = useContext(Context);
-
-    return(
-        <div>
-            <button onClick={() =>{
-                setUser(user-1);
-            }}>-</button>
-            <h1>{user}</h1>
-            <button
-            onClick={() =>{
-                setUser(user+1);
-            }}>+</button>
-        </div>
-        )
+    return (
+        <div>App</div>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import {UserContext} from "react"
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <BrowserRouter>
-    <UserContext>
+   
     <App />
-    </UserContext>
-    </BrowserRouter>
+   
 );
