906e03f10dbc8464d5a3e287e76d4dfd29919d44
Filip <filip.ratkovic.fr@gmail.com>
2023-02-15 14:40:19 +0100

    Exercise form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 // import Contact from "./contactPage/Contact";
 import Expense from "./udemy/components/first/Expense";
+import NewExpense from "./udemy/components/form/NewExpense";
 
 
 const App = () => {
   return (
     <div>
+      <NewExpense/>
       <Expense/>
     </div>
   )
